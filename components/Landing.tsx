c0f431c813ab68973c6526396753375b55907c18
shazab-saifi <shazebsaifi92@gmail.com>
2026-03-03 21:26:20 +0530

    Fixed light mode color of progressive blur decorative

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -11,7 +11,7 @@ const Landing = () => {
       {/* <BackgroundGrid /> */}
       <div
         aria-hidden
-        className="pointer-events-none fixed top-0 left-0 z-20 h-16 w-full bg-linear-to-b from-neutral-950 to-transparent mask-b-from-40% backdrop-blur-[2px] md:h-20"
+        className="pointer-events-none fixed top-0 left-0 z-20 h-16 w-full bg-linear-to-b from-neutral-100 to-transparent mask-b-from-40% backdrop-blur-[2px] md:h-20 dark:from-neutral-950"
       />
       <div className="h-[] bg-linear-to-t from-neutral-950 to-transparent" />
       <div className="flex max-w-xl flex-col gap-24 md:gap-32">
