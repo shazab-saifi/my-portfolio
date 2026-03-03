00ec1949748e87144870d7a9b2f50d9ed551f1d7
shazab-saifi <shazebsaifi92@gmail.com>
2026-03-03 21:23:24 +0530

    Added progressive blur decorative at the top

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -2,7 +2,6 @@ import HeroSection from './HeroSection';
 import ProjectSection from './ProjectSection';
 import SkillsSection from './SkillsSection';
 import ThemeToggle from './ThemeToggle';
-import BackgroundGrid from './BackgroundGrid';
 import Experience from './Experience';
 import Socials from './Socials';
 
@@ -10,6 +9,10 @@ const Landing = () => {
   return (
     <div className="scroll-content relative flex min-h-screen w-full justify-center bg-neutral-50 px-4 sm:px-20 dark:bg-neutral-950">
       {/* <BackgroundGrid /> */}
+      <div
+        aria-hidden
+        className="pointer-events-none fixed top-0 left-0 z-20 h-16 w-full bg-linear-to-b from-neutral-950 to-transparent mask-b-from-40% backdrop-blur-[2px] md:h-20"
+      />
       <div className="h-[] bg-linear-to-t from-neutral-950 to-transparent" />
       <div className="flex max-w-xl flex-col gap-24 md:gap-32">
         <HeroSection />
--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -32,7 +32,7 @@ const ThemeToggle = () => {
       <button
         type="button"
         aria-label="Toggle theme"
-        className="fixed top-4 right-4 mx-auto flex cursor-pointer items-center justify-center rounded-full border border-neutral-200 bg-white/50 p-2 shadow-md backdrop-blur-md focus:outline-none dark:border-neutral-800 dark:bg-neutral-950/50"
+        className="fixed top-4 right-4 z-30 mx-auto flex cursor-pointer items-center justify-center rounded-full border border-neutral-200 bg-white/50 p-2 shadow-md backdrop-blur-md focus:outline-none dark:border-neutral-800 dark:bg-neutral-950/50"
       >
         <IconMoon className="h-5 w-5 text-neutral-950 dark:text-neutral-100" />
       </button>
@@ -46,7 +46,7 @@ const ThemeToggle = () => {
       type="button"
       aria-label="Toggle theme"
       onClick={handleToggle}
-      className="fixed top-4 right-4 mx-auto flex cursor-pointer items-center justify-center rounded-full border border-neutral-200 bg-white/50 p-2 shadow-md backdrop-blur-md focus:outline-none dark:border-neutral-800 dark:bg-neutral-950/50"
+      className="fixed top-4 right-4 z-30 mx-auto flex cursor-pointer items-center justify-center rounded-full border border-neutral-200 bg-white/50 p-2 shadow-md backdrop-blur-md focus:outline-none dark:border-neutral-800 dark:bg-neutral-950/50"
     >
       {isDark ? (
         <IconSun className="h-6 w-6 text-neutral-950 dark:text-neutral-100" />
